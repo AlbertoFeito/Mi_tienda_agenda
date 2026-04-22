ca2247aa1f3bde8e1b9dcd30d078ddd33213e719
Alberto Feito Guerra <guerra@cid3.uim>
2026-04-22 14:47:59 -0400

    fix: base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Mi_tienda_agenda/',
   plugins: [
     react(),
     VitePWA({
