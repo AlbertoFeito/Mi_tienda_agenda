95bb495cc7a3838a94e6289551684c28e9f4d47b
Alberto Feito Guerra <guerra@cid3.uim>
2026-04-24 11:50:14 -0400

    fix: change appId to com.mitiendaagenda.app to avoid package conflict

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.mitienda.app',
+  appId: 'com.mitiendaagenda.app',
   appName: 'Mi Tienda',
   webDir: 'dist',
   bundledWebRuntime: false,
